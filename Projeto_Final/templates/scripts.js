fb9aa267e73ffeedc7d320f3115f89e248a668d6
maeve <maevenitw@gmail.com>
2023-11-29 13:17:48 -0300

    Atualizando design

--- Projeto_Final/templates/scripts.js
+++ Projeto_Final/templates/scripts.js
@@ -7,18 +7,18 @@ var statusDiv = document.getElementById("status");
 statusDiv.textContent = "Status: " + status;
 statusDiv.style.color = corDoTexto;
 
-function atualizarUmidadeManual() {
-    var inputUmidade = document.getElementById("inputUmidade");
-    var umidadeSpan = document.getElementById("umidade");
+// function atualizarUmidadeManual() {
+//     var inputUmidade = document.getElementById("inputUmidade");
+//     var umidadeSpan = document.getElementById("umidade");
 
-    var umidadeManual = parseFloat(inputUmidade.value);
-    if (!isNaN(umidadeManual)) {
-        umidadeSpan.textContent = umidadeManual;
-        verificarUmidade();
-    } else {
-        alert("Por favor, insira um valor numérico válido para a umidade.");
-    }
-}
+//     var umidadeManual = parseFloat(inputUmidade.value);
+//     if (!isNaN(umidadeManual)) {
+//         umidadeSpan.textContent = umidadeManual;
+//         verificarUmidade();
+//     } else {
+//         alert("Por favor, insira um valor numérico válido para a umidade.");
+//     }
+// }
 
 function verificarUmidade() {
     var imagemSolo = document.getElementById("imagemSolo");
