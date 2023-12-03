c805fa0fb41639b9c939cfe6549996f0cacfa8d8
Mateuspv <mateuspv2003@gmail.com>
2023-12-03 19:12:11 -0300

    Documentando back

--- Projeto_Final/API_Flask.py
+++ Projeto_Final/API_Flask.py
@@ -2,30 +2,33 @@ from flask import Flask, request, jsonify, render_template
 import psycopg2
 from flask_cors import CORS
 
+# Inicialização da aplicação Flask
 app = Flask(__name__, static_folder='templates', static_url_path='')
 CORS(app)
 
+# Configuração do banco de dados PostgreSQL
 db_host = "localhost"
 db_database = "ProjetoFinal"
 db_user = "postgres"
-db_password = "1@jean"
+db_password = "postgres"
 db_port = "5432"
 
+# Conexão ao banco de dados
 conn = psycopg2.connect(
-    database = db_database,
-    host = db_host,
-    user = db_user,
-    password = db_password,
-    port = db_port,
+    database=db_database,
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    port=db_port,
     client_encoding="utf-8"
 )
 
-
+# Rota principal que renderiza a página HTML
 @app.route('/')
 def index():
     return render_template('main.html')
 
-
+# Rota para receber dados do sensor e armazenar no banco de dados
 @app.route('/get-sensor-data', methods=['GET'])
 def post_data():
     print("Requisição recebida")
@@ -34,16 +37,17 @@ def post_data():
     tempoDesligamento = request.args.get('tempoDesligamento')
     ativadorTimer = request.args.get('ativadorTimer')
 
-
     if umidade is not None and validartroca is not None and tempoDesligamento is not None and ativadorTimer is not None:
         try:
+            # Conversão para float e inserção no banco de dados
             umidade = float(umidade)
             validartroca = float(validartroca)
             tempoDesligamento = float(tempoDesligamento)
             ativadorTimer = float(ativadorTimer)
 
             cur = conn.cursor()
-            cur.execute("INSERT INTO sensor_data (umidade, validartroca, tempoDesligamento, ativadorTimer) VALUES (%s, %s, %s, %s)", (umidade, validartroca, tempoDesligamento, ativadorTimer))
+            cur.execute("INSERT INTO sensor_data (umidade, validartroca, tempoDesligamento, ativadorTimer) VALUES (%s, %s, %s, %s)",
+                        (umidade, validartroca, tempoDesligamento, ativadorTimer))
             conn.commit()
             cur.close()
 
@@ -53,66 +57,58 @@ def post_data():
     else:
         return jsonify({'error': 'Missing data', 'message': 'All parameters are required'}), 400
 
-
-
+# Rotas para recuperar dados de validacao da troca do banco de dados
 @app.route('/get-validartroca', methods=['GET'])
 def get_validartroca():
     try:
-        # Recuperar dados do banco de dados
         cur = conn.cursor()
-        cur.execute("select validartroca from sensor_data ORDER BY id DESC LIMIT 1")
+        cur.execute("SELECT validartroca FROM sensor_data ORDER BY id DESC LIMIT 1")
         data = cur.fetchall()
         cur.close()
 
         return jsonify(data)
     except (ValueError, psycopg2.DatabaseError) as e:
-            return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
+        return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
 
+# Rotas para recuperar dados de tempo de desligamento do banco de dados
 @app.route('/get-tempoDesligamento', methods=['GET'])
 def get_tempoDesligamento():
     try:
-        # Recuperar dados do banco de dados
         cur = conn.cursor()
-        cur.execute("select tempoDesligamento from sensor_data ORDER BY id DESC LIMIT 1")
+        cur.execute("SELECT tempoDesligamento FROM sensor_data ORDER BY id DESC LIMIT 1")
         data = cur.fetchall()
         cur.close()
 
         return jsonify(data)
     except (ValueError, psycopg2.DatabaseError) as e:
-            return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
+        return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
 
+# Rotas para recuperar dados de umidade do banco de dados
 @app.route('/get-umidade', methods=['GET'])
 def get_umidade():
     try:
-        # Recuperar dados do banco de dados
         cur = conn.cursor()
-        cur.execute("select umidade from sensor_data ORDER BY id DESC LIMIT 1")
+        cur.execute("SELECT umidade FROM sensor_data ORDER BY id DESC LIMIT 1")
         data = cur.fetchall()
         cur.close()
 
         return jsonify(data)
     except (ValueError, psycopg2.DatabaseError) as e:
-            return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
+        return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
 
+# Rotas para recuperar dados de Ativar timer do banco de dados
 @app.route('/get-ativadorTimer', methods=['GET'])
 def get_ativadorTimer():
     try:
-        # Recuperar dados do banco de dados
         cur = conn.cursor()
-        cur.execute("select ativadorTimer from sensor_data ORDER BY id DESC LIMIT 1")
+        cur.execute("SELECT ativadorTimer FROM sensor_data ORDER BY id DESC LIMIT 1")
         data = cur.fetchall()
         cur.close()
 
         return jsonify(data)
     except (ValueError, psycopg2.DatabaseError) as e:
-            return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
+        return jsonify({'error': 'Bad Request', 'message': str(e)}), 400
 
-
+# Inicialização do servidor Flask
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-
-
-    
- 
--- Projeto_Final/src/main.cpp
+++ Projeto_Final/src/main.cpp
@@ -1,98 +1,115 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-#define umidadeAnalogica A0 // Atribui o pino A0 à variável umidade - leitura analógica do sensor
+// Definição do pino analógico para leitura do sensor de umidade
+#define umidadeAnalogica A0
 
-const char* ssid = "Mateus";      // Nome da sua rede Wi-Fi
-const char* password = "mat252525";  // Senha da sua rede Wi-Fi
+// Credenciais da rede Wi-Fi
+const char* ssid = "Mateus";
+const char* password = "mat252525";
 
-int valorUmidade; // Declaração da variável que armazenará o valor da umidade lida - saída analógica
-boolean validaTroca = true;
-boolean ativadorTimer = false;
-unsigned long tempoDeDesligamento;
-unsigned long DuracaoDeDesligamento = 12000;
-int tempoDesligamento = tempoDeDesligamento + DuracaoDeDesligamento - millis()/1000;
+// Variáveis globais para controle do sistema
+int valorUmidade;                 // Valor da umidade lida - saída analógica
+boolean validaTroca = true;       // Indica se a troca de estado do sistema é válida
+boolean ativadorTimer = false;    // Ativa o temporizador para desligar o sistema
+unsigned long tempoDeDesligamento; // Tempo em que o sistema foi desligado
+unsigned long DuracaoDeDesligamento = 12000; // Duração do período de desligamento (em milissegundos)
+int tempoDesligamento = tempoDeDesligamento + DuracaoDeDesligamento - millis() / 1000;
 
-WiFiServer server(80);
+WiFiServer server(80); // Inicia o servidor web na porta 80
 
 void setup() {
-  IPAddress local_IP(192,168,1,133);  // Endereço IP fixo para o ESP8266
-  IPAddress gateway(192,168,0,1);     // Gateway da rede
-  IPAddress subnet(255,255,255,0);    // Máscara de sub-rede
+  // Configuração do endereço IP fixo, gateway e máscara de sub-rede
+  IPAddress local_IP(192, 168, 1, 133);
+  IPAddress gateway(192, 168, 0, 1);
+  IPAddress subnet(255, 255, 255, 0);
 
-  Serial.begin(9600); // Inicia a comunicação serial
-  pinMode(umidadeAnalogica, INPUT); // Define umidadeAnalogica como entrada
+  // Inicia a comunicação serial
+  Serial.begin(9600);
 
+  // Configura o pino do sensor de umidade como entrada
+  pinMode(umidadeAnalogica, INPUT);
+
+  // Exibe informações sobre a conexão Wi-Fi no monitor serial
   Serial.println();
   Serial.println("Conectando-se a " + String(ssid));
 
+  // Conecta-se à rede Wi-Fi
+  WiFi.begin(ssid, password);
 
-  WiFi.begin(ssid, password);  // Conecta-se à rede Wi-Fi
-
+  // Aguarda a conexão à rede Wi-Fi
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
+  // Exibe informações sobre a conexão estabelecida
   Serial.println("");
   Serial.println("Conexão WiFi estabelecida");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
 
-  server.begin();  // Inicia o servidor web
+  // Inicia o servidor web
+  server.begin();
 }
-
 
 void loop() {
-  valorUmidade = analogRead(umidadeAnalogica); // Realiza a leitura analógica do sensor e armazena em valorumidade
-  valorUmidade = map(valorUmidade, 1023, 315, 0, 100); // Transforma os valores analógicos em uma escala de 0 a 100
+  // Realiza a leitura analógica do sensor de umidade e mapeia para uma escala de 0 a 100
+  valorUmidade = analogRead(umidadeAnalogica);
+  valorUmidade = map(valorUmidade, 1023, 315, 0, 100);
 
-if (validaTroca == true){
-  if (valorUmidade >= 40){
-  Serial.print("Solo Umido ");
-  Serial.print(valorUmidade);
-  Serial.println(" %");
-}else{
-  Serial.println("Ligado Sprinter");
-  validaTroca = false;
+  // Controle de umidade e acionamento do sistema
+  if (validaTroca == true) {
+    if (valorUmidade >= 40) {
+      Serial.print("Solo Úmido ");
+      Serial.print(valorUmidade);
+      Serial.println(" %");
+    } else {
+      Serial.println("Ligado Sprinter");
+      validaTroca = false;
+    }
   }
-}
-if (validaTroca == false){
-  if (valorUmidade < 40){
-    Serial.print("Solo Seco ");
-    Serial.print(valorUmidade);
-    Serial.println(" %, Agua Ligada");
-  }
-  else if(valorUmidade > 40 && ativadorTimer == false){
-    Serial.println("Solo está umido, Desativando Sprinter");
-    tempoDeDesligamento = millis();
-    ativadorTimer = true; 
-  }
-}
 
-if(ativadorTimer == true){
-  if(tempoDeDesligamento + DuracaoDeDesligamento < millis()){
-  validaTroca = true;
-  ativadorTimer = false;
+  // Controle de irrigação e temporizador
+  if (validaTroca == false) {
+    if (valorUmidade < 40) {
+      Serial.print("Solo Seco ");
+      Serial.print(valorUmidade);
+      Serial.println(" %, Água Ligada");
+    } else if (valorUmidade > 40 && ativadorTimer == false) {
+      Serial.println("Solo está úmido, Desativando Sprinter");
+      tempoDeDesligamento = millis();
+      ativadorTimer = true; 
+    }
   }
-  else{
-    Serial.println("Desligando Sprinter em:");
-    Serial.println((tempoDeDesligamento + DuracaoDeDesligamento - millis())/1000);
+
+  // Temporizador para desligar o sistema
+  if (ativadorTimer == true) {
+    if (tempoDeDesligamento + DuracaoDeDesligamento < millis()) {
+      validaTroca = true;
+      ativadorTimer = false;
+    } else {
+      Serial.println("Desligando Sprinter em:");
+      Serial.println((tempoDeDesligamento + DuracaoDeDesligamento - millis()) / 1000);
+    }
   }
-}
 
-WiFiClient client;
+  // Conexão ao servidor web e envio de dados
+  WiFiClient client;
 
-String url = "/get-sensor-data?umidade=" + String(valorUmidade) + "&validartroca=" + String(validaTroca) + "&tempoDesligamento=" + String(tempoDesligamento) + "&ativadorTimer=" + String(ativadorTimer) ;
+  // Construção da URL com os dados relevantes
+  String url = "/get-sensor-data?umidade=" + String(valorUmidade) + "&validartroca=" + String(validaTroca) + "&tempoDesligamento=" + String(tempoDesligamento) + "&ativadorTimer=" + String(ativadorTimer);
 
-  if (client.connect("192.168.0.101", 5000)) {  // Conecta-se ao servidor web
+  // Conecta-se ao servidor web
+  if (client.connect("192.168.0.101", 5000)) {
     Serial.println("Conectado ao servidor");
 
+    // Envia uma requisição GET HTTP para o servidor com a URL construída
     client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                  "Host: 192.168.0.101\r\n" +
                  "Connection: close\r\n\r\n");
-    // Envia uma requisição GET HTTP para o servidor com a URL construída
 
+    // Aguarda e exibe a resposta do servidor no monitor serial
     while (client.connected()) {
       if (client.available()) {
         String line = client.readStringUntil('\r');
@@ -100,15 +117,12 @@ String url = "/get-sensor-data?umidade=" + String(valorUmidade) + "&validartroca
       }
     }
 
-    client.stop();  // Encerra a conexão com o servidor web
+    // Encerra a conexão com o servidor web
+    client.stop();
   } else {
-    Serial.println("Connection to server failed");
+    Serial.println("Falha na conexão com o servidor");
   }
 
-  delay(1000);  // Aguarda 1 segundo antes de enviar os dados novamente
+  // Aguarda 1 segundo antes de enviar os dados novamente
+  delay(1000);
 }
-
-
-
-
-
